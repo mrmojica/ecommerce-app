c4b8e29df54ed670ebfea5de7a966d257bdcd6b5
mrmojica <mrmojica510@gmail.com>
2020-02-29 12:19:03 -0800

    WIP add homepage skeleton w/ styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Homepage from "./pages/Homepage/Homepage";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
