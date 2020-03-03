a83e55884f937c87ab254746052650054fa0cdf4
mrmojica <mrmojica510@gmail.com>
2020-03-02 17:43:37 -0800

    add initial react-router basic setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage";
@@ -6,7 +8,9 @@ import Homepage from "./pages/Homepage/Homepage";
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
