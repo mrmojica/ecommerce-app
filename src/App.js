32e3b83c79beb5721ec2221b55b9cc4143d649a0
mrmojica <mrmojica510@gmail.com>
2020-03-03 18:08:41 -0800

    add Shop route to App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   );
