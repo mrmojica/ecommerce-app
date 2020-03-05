a312e1fdc8d08597ece1b766310f561390301a25
mrmojica <mrmojica510@gmail.com>
2020-03-05 15:25:36 -0800

    SignIn - apply custom form inputs

---
+++ src/pages/SignInAndSignUp/SignIn/SignIn.js
@@ -0,0 +1,58 @@
+import React from "react";
+import FormInput from "../../../components/FormInput/FormInput";
+
+import "./styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmite = event => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password </span>
+
+        <form>
+          <FormInput
+            name="email"
+            type="text"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -1,9 +1,12 @@
 import React from "react";
+import SignIn from "./SignIn//SignIn";
 
 import "./styles.scss";
 
 const SignInAndSignUp = () => (
-  <div className="sign-in-and-sign-up">Sign In</div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUp;
