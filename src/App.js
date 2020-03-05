37493830bb23671c0bd161b944e821c19a596829
mrmojica <mrmojica510@gmail.com>
2020-03-05 14:56:54 -0800

    create initial signup page and route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage";
 import Shop from "./pages/Shop/Shop";
+import SignInAndSignUp from "./pages/SignInAndSignUp/SignInAndSignUp";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.scss";
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">Sign In</div>
+);
+
+export default SignInAndSignUp;
