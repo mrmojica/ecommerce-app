9508e78a569b89ed61193c83e1615bf5d9f0b176
mrmojica <mrmojica510@gmail.com>
2020-03-10 17:58:45 -0700

    import SignUp to SignInAndSignUp page and add styles

--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SignIn from "./SignIn//SignIn";
+import SignUp from "./SignUp/SignUp";
 
 import "./styles.scss";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
