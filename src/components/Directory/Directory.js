c6e67d3e000fbd542b644a89cef4116adc61e61b
mrmojica <mrmojica510@gmail.com>
2020-03-20 10:34:39 -0700

    move directory state to redux

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,57 +1,20 @@
 import React from "react";
 import MenuItem from "../MenuItem/MenuItem";
+import { connect } from "react-redux";
+import { selectDirectorySection } from "../../redux/directory/directorySelectors";
+import { createStructuredSelector } from "reselect";
 import "./styles.scss";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...props }) => (
+      <MenuItem key={id} {...props} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats"
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets"
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers"
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens"
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens"
-        }
-      ]
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...props }) => (
-          <MenuItem key={id} {...props} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySection
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
