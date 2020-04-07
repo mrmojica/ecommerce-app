4b2f3f13ea192dc0794f51d4729d2eeb85a503ca
mrmojica <mrmojica510@gmail.com>
2020-04-07 15:57:01 -0700

    remove unneeded div and className

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -4,10 +4,10 @@ import { Route } from "react-router-dom";
 import Collection from "../Collection/Collection";
 
 const Shop = ({ match }) => (
-  <div className="shop-page">
+  <>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={Collection} />
-  </div>
+  </>
 );
 
 export default Shop;
