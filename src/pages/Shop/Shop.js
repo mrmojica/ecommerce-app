777156f26e6a449b0bb7af8398dec825ff2cd94c
mrmojica <mrmojica510@gmail.com>
2020-04-13 15:58:52 -0700

    convert from observer pattern to promise pattern

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -24,13 +24,11 @@ class Shop extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubScribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
