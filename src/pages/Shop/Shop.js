6206dbc185772ec095cc5c78009a9c2e90f759b7
mrmojica <mrmojica510@gmail.com>
2020-05-04 17:54:18 -0700

    remove thunk and test shopSaga

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
-import { fetchCollectionsStartAsync } from "../../redux/shop/shopActions";
+import { fetchCollectionStart } from "../../redux/shop/shopActions";
 import CollectionsOverviewContainer from "../../components/CollectionsOverview/container/CollectionsOverviewContainer";
 import CollectionContainer from "../Collection/container/CollectionContainer";
 
 class Shop extends React.Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
+    const { fetchCollectionStart } = this.props;
+    fetchCollectionStart();
   }
 
   render() {
@@ -31,7 +31,7 @@ class Shop extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionStart: () => dispatch(fetchCollectionStart())
 });
 
 export default connect(null, mapDispatchToProps)(Shop);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { fetchCollectionsStart } from "./shop/shopSagas";
 
 import rootReducer from "./rootReducer";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
@@ -13,4 +16,8 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
