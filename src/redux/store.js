953ec297a242d07799466741260d016d642c45c5
mrmojica <mrmojica510@gmail.com>
2020-05-14 15:12:53 -0700

    add rootSaga to handle/run all sagas at once

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
-import { fetchCollectionsStart } from "./shop/shopSagas";
 
 import rootReducer from "./rootReducer";
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
