4254cc7dda1ce6b036f4d74e8d99ef49d25fc31d
mrmojica <mrmojica510@gmail.com>
2020-06-04 17:31:23 -0700

    add note

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -3,5 +3,6 @@ import { fetchCollectionsStart } from "./shop/shopSagas";
 import { userSagas } from "./user/userSaga";
 
 export default function* rootSaga() {
+  // initial all tasks at once.
   yield all([call(fetchCollectionsStart), call(userSagas)]);
 }
