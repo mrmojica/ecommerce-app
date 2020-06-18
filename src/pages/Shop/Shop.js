9fd3a4b8e9f316b84c2b6d8ead89f9b0cf35785d
mrmojica <mrmojica510@gmail.com>
2020-06-17 17:12:40 -0700

    convert Shop to use hooks

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchCollectionsStart } from "../../redux/shop/shopActions";
 import CollectionsOverviewContainer from "../../components/CollectionsOverview/container/CollectionsOverviewContainer";
 import CollectionContainer from "../Collection/container/CollectionContainer";
 
-class Shop extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const Shop = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-
-    return (
-      <>
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionContainer}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionContainer}
+      />
+    </>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
