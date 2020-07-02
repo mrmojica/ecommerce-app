596c367507bbc5c8d9df60fbe97f2046eb62956e
mrmojica <mrmojica510@gmail.com>
2020-07-02 15:11:47 -0700

    test if hardcoding firebaseConfig fixes heroku issue

--- client/src/Firebase/utils.js
+++ client/src/Firebase/utils.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyD5UShtnOc3JnUORwDTfHcNsHxPI54Q2O0",
+  authDomain: "ecommerce-db-aa529.firebaseapp.com",
+  databaseURL: "https://ecommerce-db-aa529.firebaseio.com",
+  projectId: "ecommerce-db-aa529",
+  storageBucket: "ecommerce-db-aa529.appspot.com",
+  messagingSenderId: "903310405557",
+  appId: "1:903310405557:web:320b9ff38259f0b071b3fc",
+  measurementId: "G-BXT10TMLZQ"
 };
 
 // firebase.User reference
