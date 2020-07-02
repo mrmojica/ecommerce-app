09c5f8506f5e0afad418370875067b07d1f39cf9
mrmojica <mrmojica510@gmail.com>
2020-07-02 11:42:34 -0700

    comment for adding compression

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Heroku does not offer Gzipping compression so
+// we added compression library to resolve this.
 app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
